ccfe70943f4e73d43efc0c8315aeba025bbb33db
test user <test@mail.ru>
2025-04-03 16:21:43 +0300

    Initialized project

---
+++ src/test/java/com/Danthedev/Tradebot/TradebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Danthedev.Tradebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
