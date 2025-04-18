119ec20a31fdc7609a32fa7b766a2a2688cdc43d
test user <test@mail.ru>
2025-04-18 12:11:38 +0300

    TradeBot v1.3 ready for local development

--- src/main/java/com/Danthedev/Tradebot/TradebotApplication.java
+++ src/main/java/com/Danthedev/Tradebot/TradebotApplication.java
@@ -1,5 +1,6 @@
 package com.Danthedev.Tradebot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,14 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class TradebotApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach(entry -> {
+					if (System.getenv(entry.getKey()) == null && System.getProperty(entry.getKey()) == null) {
+						System.setProperty(entry.getKey(), entry.getValue());
+					}
+				});
+
 		SpringApplication.run(TradebotApplication.class, args);
 	}
 
